e13102fdcddf674debb8bbe0d5c43bd940c2d045
Sidney Andrews <sidney@seesharprun.net>
2017-05-15 15:17:54 -0400

    Format HomeController

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,8 +2,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace HealthGuide.API.Metadata.Controllers
 {
-    public class HomeController : Controller {
-        
+    public class HomeController : Controller 
+    {        
         public IActionResult Index()
         {
             return View();
